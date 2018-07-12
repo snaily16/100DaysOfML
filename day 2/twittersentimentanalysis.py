1b6fce25f039eba56da76bd3d2a36965bbd5a198
snaily16 <snaily16@gmail.com>
2018-07-13 00:18:29 +0530

    Day 3 work

--- day 2/twittersentimentanalysis.py
+++ day 2/twittersentimentanalysis.py
@@ -12,10 +12,29 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.search('100DaysOfMLCode')
+def get_label():
+	return input('Enter label: ')
 
-for tweet in public_tweets:
-	print(tweet.text)
-	analysis = TextBlob(tweet.text)
-	print(analysis.sentiment)
-	print("")
+def get_sentiment(tweets):
+	analysis = TextBlob(tweets)
+	if analysis.sentiment.polarity > 0:
+		return 'positive'
+	elif analysis.sentiment.polarity < 0:
+		return 'negative'
+	else:
+		return 'neutral'
+
+data=[]
+name = get_label()
+public_tweets = api.search(name, count=100)
+parsed_tweet ={'text':'NaN','user':'NaN','sentiment':'NaN'}
+
+with open('%s_tweets.csv' % name, 'w') as tweets_data:
+	writer = csv.DictWriter(tweets_data, parsed_tweet.keys())
+	writer.writeheader()
+	for tweet in public_tweets:
+		parsed_tweet['text']=tweet.text
+		parsed_tweet['user']=tweet.user.screen_name
+		parsed_tweet['sentiment'] = get_sentiment(tweet.text)
+		writer.writerow(parsed_tweet)
+
